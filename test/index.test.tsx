33bde225461c366147dcc01564fb6f1f5c76e394
Yago Mfh <yagomfh@gmail.com>
2024-04-23 14:51:08 +0200

    feat: first commit

---
+++ test/index.test.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { EmailLoginForm } from '../src/index';
+
+describe('EmailLogin', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <EmailLoginForm
+        buttonLabel="Submit"
+        emailLabel="Email"
+        passwordLabel="Password"
+        onSubmit={async values => console.log(values)}
+      />,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
